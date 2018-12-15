45c43c093ea1933add4fc03954dce8c6a383d8b8
blasanka <blasanka95@gmail.com>
2018-12-15 23:03:29 +0530

    got firebase to working

---
+++ app/Http/Controllers/FirebaseController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Kreait\Firebase;
+use Kreait\Firebase\Factory;
+use Kreait\Firebase\ServiceAccount;
+
+class FirebaseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $serviceAccount = ServiceAccount::fromJsonFile(__DIR__.'/adapp-df2ae-bff1ac7f17ad.json');
+        $firebase = (new Factory)
+        ->withServiceAccount($serviceAccount)
+        ->withDatabaseUri('https://adapp-df2ae.firebaseio.com/')
+        ->create();
+
+        $database = $firebase->getDatabase();
+
+        $newPost = $database
+        ->getReference('addapp/posts')
+        ->push([
+        'title' => 'Laravel FireBase Tutorial' ,
+        'category' => 'Laravel'
+        ]);
+        echo '<pre>';
+        print_r($newPost->getvalue());
+    }
+
+}
